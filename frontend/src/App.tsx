d1aa3d344793666a7631fd74ab7599db3d4cde5d
Sunny-jinn <rlawlsdn316@gmail.com>
2023-10-23 16:49:34 +0900

    feat: router 세팅 & 메인 페이지 라우팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <>
       <Header />
-      <div>빵팅</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
