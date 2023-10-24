88f58c03994fa736ca58fc2d58dc4064c0d375c7
Sunny-jinn <rlawlsdn316@gmail.com>
2023-10-24 23:48:53 +0900

    feat: 빵케팅 라인업 추가

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,35 @@
+import { Container, Grid, Typography } from '@mui/material';
 import { MainBanner } from '../components/Home/MainBanner';
 
 export const Home = () => {
   return (
     <>
       <MainBanner />
-      <div>홈 </div>
+      <Container fixed style={{ marginTop: 40, padding: 0 }}>
+        <Typography
+          variant="h5"
+          sx={{ fontWeight: 600, marginLeft: 5, marginBottom: 5 }}
+        >
+          빵케팅 라인업
+        </Typography>
+        <Grid container>
+          <Grid
+            item
+            xs={4}
+            display={'flex'}
+            direction="column"
+            alignItems={'center'}
+            mb={5}
+          >
+            <img
+              src="/imgs/soboro.png"
+              alt=".."
+              style={{ width: 320, height: 250, marginBottom: 10 }}
+            />
+            <Typography sx={{ fontSize: 20 }}>[성심당] 튀김소보로</Typography>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
