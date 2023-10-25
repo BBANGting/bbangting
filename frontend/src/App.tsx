3e83957be8b21bdfedeeb8d441c644d8ed956a09
Sunny-jinn <rlawlsdn316@gmail.com>
2023-10-25 23:56:21 +0900

    feat: 로그인, 회원가입 페이지 라우팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Signup } from './pages/Signup';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
