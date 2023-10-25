b9b3b8109e48077bda13a8e6132c9455681a542a
Sunny-jinn <rlawlsdn316@gmail.com>
2023-10-26 02:51:27 +0900

    feat: 이메일 유효성 검사

--- frontend/src/components/Login/InputBox.tsx
+++ frontend/src/components/Login/InputBox.tsx
@@ -1,4 +1,5 @@
 import { Typography, TextField } from '@mui/material';
+import { useState } from 'react';
 
 type InputBoxProps = {
   name: string;
@@ -7,12 +8,33 @@ type InputBoxProps = {
 };
 
 export const InputBox = ({ name, holder, type }: InputBoxProps) => {
+  const [isError, setIsError] = useState<boolean>(false);
+  const [inputEmail, setInputEmail] = useState<string>('');
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setInputEmail(e.target.value);
+    setIsError(!confirmEmail);
+  };
+
+  const confirmEmail: boolean = /^[\w+_]\w+@\w+\.\w+/.test(inputEmail);
+
   return (
     <>
       <Typography variant="subtitle1" mb={0.5}>
         {name}
       </Typography>
-      <TextField type={type} fullWidth placeholder={holder} sx={{ mb: 3.5 }} />
+      <TextField
+        required
+        onChange={inputHandler}
+        onKeyDown={() => setIsError(true)}
+        error={isError}
+        type={type}
+        fullWidth
+        placeholder={holder}
+        sx={{ mb: 3.5 }}
+        helperText="d"
+      />
     </>
   );
 };
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,18 @@
-import { Box, Container, Typography } from '@mui/material';
-import { InputBox } from '../components/Login/InputBox';
+import { Box, Container, Grid, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
 
 export const Login = () => {
+  const [isError, setIsError] = useState<boolean>(false);
+  const [inputEmail, setInputEmail] = useState<string>('');
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setInputEmail(e.target.value);
+    setIsError(!confirmEmail);
+  };
+
+  const confirmEmail: boolean = /^[\w+_]\w+@\w+\.\w+/.test(inputEmail);
+
   return (
     <Container
       sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
@@ -10,8 +21,25 @@ export const Login = () => {
         로그인
       </Typography>
       <Box style={{ width: 400 }}>
-        <InputBox name="이메일" holder="이메일 입력" />
-        <InputBox name="비밀번호" holder="비밀번호 입력" type="password" />
+        <Grid sx={{ height: 125 }}>
+          <Typography variant="subtitle1" mb={0.5}>
+            이메일
+          </Typography>
+          <TextField
+            onChange={inputHandler}
+            onKeyDown={() => setIsError(true)}
+            error={isError}
+            fullWidth
+            placeholder="이메일 입력"
+            helperText={isError && '이메일 형식을 맞춰주세요.'}
+          />
+        </Grid>
+        <Grid sx={{ height: 125 }}>
+          <Typography variant="subtitle1" mb={0.5}>
+            비밀번호
+          </Typography>
+          <TextField fullWidth placeholder="비밀번호 입력" />
+        </Grid>
       </Box>
     </Container>
   );
