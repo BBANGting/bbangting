13632f16bdd94332f1c94be48d4007fff906d466
Sunny-jinn <rlawlsdn316@gmail.com>
2023-10-30 20:23:55 +0900

    feat: 헤더 메뉴 추가 및 메뉴 주소 연결

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,6 +3,7 @@ import {
   AppBar,
   Box,
   IconButton,
+  MenuItem,
   ThemeProvider,
   Toolbar,
   Typography,
@@ -10,6 +11,29 @@ import {
   useMediaQuery,
 } from '@mui/material';
 import { ReactElement, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const leftOptions = [
+  {
+    name: '오픈 예정',
+    link: '/commingsoon',
+  },
+  {
+    name: '스토어',
+    link: '/store',
+  },
+];
+
+const rightOptions = [
+  {
+    name: '로그인',
+    link: '/login',
+  },
+  {
+    name: '회원가입',
+    link: '/signup',
+  },
+];
 
 const theme = createTheme({
   palette: {
@@ -32,31 +56,38 @@ export const Header = (): ReactElement => {
     <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" color="header" sx={{ placeItems: 'center' }}>
-          <Toolbar sx={{ minWidth: width ? 1000 : 1440 }}>
-            <Typography
-              variant="h6"
-              component={'div'}
-              sx={{ flexGrow: 1, fontWeight: 600 }}
-            >
-              BBANGTING
-            </Typography>
+          <Toolbar sx={{ minWidth: width ? 1080 : 1440 }} disableGutters>
+            <Link to={`/`}>
+              <Typography
+                variant="h6"
+                component={'div'}
+                sx={{ flexGrow: 0, fontWeight: 600, fontSize: 24, mr: 5 }}
+              >
+                BBANGTING
+              </Typography>
+            </Link>
+            <Box sx={{ flexGrow: 1, display: 'flex' }}>
+              {leftOptions.map(menu => (
+                <Link to={menu.link}>
+                  <MenuItem sx={{ borderRadius: 5 }}>
+                    <Typography variant="h6" sx={{ fontWeight: 400 }}>
+                      {menu.name}
+                    </Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </Box>
             {!isLogin ? (
               <>
-                <Typography
-                  variant="h6"
-                  component={'div'}
-                  sx={{ flexGrow: 0, fontWeight: 400 }}
-                  mr={3}
-                >
-                  로그인
-                </Typography>
-                <Typography
-                  variant="h6"
-                  component={'div'}
-                  sx={{ flexGrow: 0, fontWeight: 400 }}
-                >
-                  회원가입
-                </Typography>
+                {rightOptions.map(menu => (
+                  <Link to={menu.link}>
+                    <MenuItem sx={{ borderRadius: 5 }}>
+                      <Typography variant="h6" sx={{ fontWeight: 400 }}>
+                        {menu.name}
+                      </Typography>
+                    </MenuItem>
+                  </Link>
+                ))}
               </>
             ) : (
               <>
