1233bdd75108cef08dfa2e04be7f64d8567f4a6d
Sungah <sungah0815@gmail.com>
2023-10-31 16:57:59 +0900

    feat: spring security config 추가 설정

--- backend/src/main/java/com/khu/bbangting/config/SecurityConfig.java
+++ backend/src/main/java/com/khu/bbangting/config/SecurityConfig.java
@@ -1,10 +1,7 @@
 package com.khu.bbangting.config;
 
-import com.khu.bbangting.service.UserDetailService;
-import com.khu.bbangting.service.UserService;
 import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,7 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
--- backend/src/main/java/com/khu/bbangting/config/auth/PrincipalDetailService.java
+++ backend/src/main/java/com/khu/bbangting/config/auth/PrincipalDetailService.java
@@ -1,8 +1,9 @@
-package com.khu.bbangting.service;
+package com.khu.bbangting.config.auth;
 
 import com.khu.bbangting.model.User;
 import com.khu.bbangting.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,14 +11,18 @@ import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class UserDetailService implements UserDetailsService {
+public class PrincipalDetailService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
-    public User loadUserByUsername(String email) {
-        return userRepository.findByEmail(email)
+    public UserDetails loadUserByUsername(String email) {
+
+        User principal = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾지 못했니다. username : " + email));
+
+        return new PrincipalDetail(principal);
     }
 
 }
