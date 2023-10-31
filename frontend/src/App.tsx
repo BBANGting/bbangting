f00de70e4445eb6d41a1d62358a06deea232cbdf
Sunny-jinn <rlawlsdn316@gmail.com>
2023-11-01 02:10:30 +0900

    feat: 스토어 페이지 라우팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { CommingSoon } from './pages/CommingSoon';
+import { Store } from './pages/Store';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/commingsoon" element={<CommingSoon />} />
+        <Route path="/store" element={<Store />} />
       </Routes>
     </>
   );
