98cec91c1c712b551d3244487219109aeaf050b1
Sungah <sungah0815@gmail.com>
2023-11-14 14:19:44 +0900

    rename: BreadController 파일명 수정

--- backend/src/main/java/com/khu/bbangting/controller/BreadApiController.java
+++ backend/src/main/java/com/khu/bbangting/controller/BreadApiController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-public class BreadController {
+public class BreadApiController {
 
     @Autowired
     private BreadService breadService;
