a2c4f1926b9552389a47c1c90e0e1abd9aa202c0
Sunny-jinn <rlawlsdn316@gmail.com>
2023-11-27 23:21:57 +0900

    feat: 스토어 상세 정보 추가

--- frontend/src/pages/StoreDetail.tsx
+++ frontend/src/pages/StoreDetail.tsx
@@ -2,6 +2,7 @@ import { Container } from '@mui/material';
 import temp_store from '../components/json/store.json';
 import { useParams } from 'react-router-dom';
 import StoreBanner from '../components/StoreDetail/StoreBanner';
+import StoreInfo from '../components/StoreDetail/StoreInfo';
 
 const StoreDetail: React.FC = () => {
   const { storeId } = useParams<string>();
@@ -11,6 +12,7 @@ const StoreDetail: React.FC = () => {
   return (
     <Container disableGutters>
       <StoreBanner storeImage={store.storeImage} storeLogo={store.storeLogo} />
+      <StoreInfo store={store} />
     </Container>
   );
 };
