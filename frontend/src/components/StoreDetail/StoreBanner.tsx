0eb370db4a9a88f4a3ff7059c7499962eae6dacd
Sunny-jinn <rlawlsdn316@gmail.com>
2023-11-27 20:42:12 +0900

    feat: 스토어 상세 페이지 상단 배너 추가
    배경 사진과 로고 사진 추가

---
+++ frontend/src/components/StoreDetail/StoreBanner.tsx
@@ -0,0 +1,33 @@
+import { Grid } from '@mui/material';
+
+type StoreBannerProps = {
+  storeImage: string;
+  storeLogo: string;
+};
+
+const StoreBanner: React.FC<StoreBannerProps> = ({ storeImage, storeLogo }) => {
+  return (
+    <Grid position="relative" width="100%" maxHeight={372}>
+      <img
+        src={storeImage}
+        alt="스토어 배경"
+        width="100%"
+        style={{ objectFit: 'cover', minHeight: 270 }}
+      />
+      <img
+        src={storeLogo}
+        alt="스토어 로고"
+        width={130}
+        height={130}
+        style={{
+          position: 'absolute',
+          bottom: 20,
+          left: 20,
+          borderRadius: 999,
+        }}
+      />
+    </Grid>
+  );
+};
+
+export default StoreBanner;
