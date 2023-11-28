bd22ee8fed71e080adb77e1a71f8cf6bb7b6f2be
Sungah <sungah0815@gmail.com>
2023-11-29 02:29:45 +0900

    feat: 프론트 request 포트 번호 허용

--- backend/src/main/java/com/khu/bbangting/config/WebMvcConfig.java
+++ backend/src/main/java/com/khu/bbangting/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.khu.bbangting.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +16,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations(uploadPath);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3000);
     }
 
 }
