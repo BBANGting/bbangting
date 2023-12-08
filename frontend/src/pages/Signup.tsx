0e865d54be7622c2556438fd98f00ec3626ff429
Sunny-jinn <rlawlsdn316@gmail.com>
2023-12-08 17:24:47 +0900

    feat: 회원가입 api 연동

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,10 +1,29 @@
 import { Box, Container, Grid, TextField, Typography } from '@mui/material';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { AuthButton } from '../components/Login/AuthButton';
+import axios from 'axios';
 
 export const Signup = () => {
   const [isError, setIsError] = useState<boolean>(false);
+
   const [inputEmail, setInputEmail] = useState<string>('');
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const nicknameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const pwdRef = useRef<HTMLInputElement>(null);
+
+  const clickHandler = () => {
+    axios
+      .post('http://localhost:8080/auth/join', {
+        username: nameRef.current.value,
+        nickname: nicknameRef.current.value,
+        email: emailRef.current.value,
+        password: pwdRef.current.value,
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
 
   const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -27,7 +46,13 @@ export const Signup = () => {
           <Typography variant="subtitle1" mb={0.5}>
             이름
           </Typography>
-          <TextField fullWidth placeholder="이름 입력" />
+          <TextField fullWidth placeholder="이름 입력" inputRef={nameRef} />
+        </Grid>
+        <Grid sx={{ height: 125 }}>
+          <Typography variant="subtitle1" mb={0.5}>
+            닉네임
+          </Typography>
+          <TextField fullWidth placeholder="이름 입력" inputRef={nicknameRef} />
         </Grid>
         <Grid sx={{ height: 125 }}>
           <Typography variant="subtitle1" mb={0.5}>
@@ -39,13 +64,19 @@ export const Signup = () => {
             fullWidth
             placeholder="이메일 입력"
             helperText={isError && '이메일 형식을 맞춰주세요.'}
+            inputRef={emailRef}
           />
         </Grid>
         <Grid sx={{ height: 125 }}>
           <Typography variant="subtitle1" mb={0.5}>
             비밀번호
           </Typography>
-          <TextField fullWidth placeholder="비밀번호 입력" type="password" />
+          <TextField
+            fullWidth
+            placeholder="비밀번호 입력"
+            type="password"
+            inputRef={pwdRef}
+          />
         </Grid>
         <Grid sx={{ height: 125 }}>
           <Typography variant="subtitle1" mb={0.5}>
@@ -59,7 +90,7 @@ export const Signup = () => {
           />
         </Grid>
         <Grid mt={3}>
-          <AuthButton text="회원가입 완료" />
+          <AuthButton text="회원가입 완료" onClick={clickHandler} />
         </Grid>
       </Box>
     </Container>
