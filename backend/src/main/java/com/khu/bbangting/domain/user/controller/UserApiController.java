8f52db52b32439af6fcefc1f3a2c58ffde4589ca
Hyeyeon-Ko <khy33355@gmail.com>
2023-12-10 01:12:44 +0900

    refactor: 로그아웃 api 삭제, refresh token 생성

--- backend/src/main/java/com/khu/bbangting/domain/user/controller/UserApiController.java
+++ backend/src/main/java/com/khu/bbangting/domain/user/controller/UserApiController.java
@@ -4,11 +4,14 @@ import com.khu.bbangting.domain.user.dto.*;
 import com.khu.bbangting.domain.user.model.User;
 import com.khu.bbangting.domain.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,22 +22,23 @@ public class UserApiController {
     // 회원가입
     @PostMapping("/auth/join")
     public ResponseEntity<UserResponseDto> joinUser(@RequestBody JoinRequestDto joinRequestDto) {
-        UserResponseDto successUser = userService.join(joinRequestDto);
+        userService.join(joinRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     // 로그인
     @PostMapping("/auth/login")
     public ResponseEntity<UserTokenDto> login(@RequestBody LoginRequestDto requestDto) {
-        UserTokenDto loginDto = userService.login(requestDto);
-        return ResponseEntity.status(HttpStatus.OK).header(loginDto.getToken()).body(loginDto);
+        User user = User.builder()
+                .email(requestDto.getEmail())
+                .password(requestDto.getPassword())
+                .build();
+        return ResponseEntity.ok(userService.login(user));
     }
 
-//    // 로그아웃
-//    @PostMapping("/logout")
-//    public ResponseEntity<Void> logout() {
-//        userService.logout();
-//        return ResponseEntity.ok().build();
-//    }
+    @PostMapping("/auth/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        userService.refreshToken(request, response);
+    }
 
 }
