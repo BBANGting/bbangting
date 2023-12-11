7dbf902d5b6983759842747b7dc4a92568f4f565
Hyeyeon-Ko <khy33355@gmail.com>
2023-12-11 15:54:04 +0900

    refactor: 주문내역 불러올 때 status 확인 추가

--- backend/src/main/java/com/khu/bbangting/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/com/khu/bbangting/domain/order/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.khu.bbangting.domain.order.repository;
 
 import com.khu.bbangting.domain.bread.model.Bread;
 import com.khu.bbangting.domain.order.model.Order;
+import com.khu.bbangting.domain.order.model.OrderStatus;
 import com.khu.bbangting.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,7 +10,7 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    List<Order> findAllByUserId(Long userId);
+    List<Order> findAllByUserIdAndOrderStatus(Long userId, OrderStatus orderStatus);
 
     boolean existsByBreadAndUser(Bread bread, User user);
 
