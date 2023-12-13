4c775cba1b8b80600089b01ba7fea3e81b65e8f3
Sunny-jinn <rlawlsdn316@gmail.com>
2023-12-14 02:29:19 +0900

    feat: store 조회 custom api 추가

--- frontend/src/pages/StoreDetail.tsx
+++ frontend/src/pages/StoreDetail.tsx
@@ -1,24 +1,30 @@
 import { Container } from '@mui/material';
-import temp_store from '../components/json/store.json';
-import temp_bread from '../components/json/bread.json';
 import { useParams } from 'react-router-dom';
 import StoreBanner from '../components/StoreDetail/StoreBanner';
 import StoreInfo from '../components/StoreDetail/StoreInfo';
 import StoreBreads from '../components/StoreDetail/StoreBreads';
+import { getStoreInfo } from '../apis/api/storeInfo';
+import { useEffect, useState } from 'react';
+import { TStoreInfo } from '../types';
 
 const StoreDetail: React.FC = () => {
+  const [store, setStore] = useState<TStoreInfo>({} as TStoreInfo);
+  const [breadList, setBreadList] = useState([]);
+
   const { storeId } = useParams<string>();
+  useEffect(() => {
+    getStoreInfo(Number(storeId)).then(res => {
+      console.log(res);
+      setStore(res.storeInfo);
+      setBreadList(res.breadList);
+    });
+  }, []);
 
-  const store = temp_store.filter(
-    store => store.storeId === Number(storeId),
-  )[0];
-
-  const bread = temp_bread.filter(bread => bread.storeId === Number(storeId));
   return (
     <Container disableGutters>
-      <StoreBanner storeImage={store.storeImage} storeLogo={store.storeLogo} />
+      <StoreBanner storeImage={store.imgUrl2} storeLogo={store.imgUrl} />
       <StoreInfo store={store} />
-      <StoreBreads bread={bread} />
+      <StoreBreads bread={breadList} />
     </Container>
   );
 };
