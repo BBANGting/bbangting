64edd3c22a1f971ca436b6d2a96fe8a777f1b227
Sunny-jinn <rlawlsdn316@gmail.com>
2023-12-15 02:24:56 +0900

    fix: instance header token update 안 되는 버그 수정

--- frontend/src/apis/utils/instance.ts
+++ frontend/src/apis/utils/instance.ts
@@ -1,4 +1,10 @@
-import axios, { AxiosHeaders, AxiosRequestConfig } from 'axios';
+import axios, {
+  AxiosError,
+  AxiosHeaders,
+  AxiosRequestConfig,
+  AxiosRequestHeaders,
+  AxiosResponse,
+} from 'axios';
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
@@ -8,18 +14,41 @@ const axiosApi = (url: string, options?: AxiosRequestConfig) => {
     ...options,
     timeout: 2000,
   });
+
+  instance.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    (error: AxiosError) => {
+      return Promise.reject(error);
+    },
+  );
+
   return instance;
 };
 
 const axiosAuthApi = (url: string, options?: AxiosHeaders) => {
-  const token = localStorage.getItem('access-token');
   const instance = axios.create({
     baseURL: url,
     headers: {
-      Authorization: `Bearer ${token}`,
       ...options,
     },
   });
+
+  instance.interceptors.request.use(
+    config => {
+      const token = localStorage.getItem('access-token');
+      if (token) {
+        config.headers = config.headers || {};
+        (
+          config.headers as AxiosRequestHeaders
+        ).Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    error => {
+      console.error(error);
+      return Promise.reject(error);
+    },
+  );
   return instance;
 };
 
