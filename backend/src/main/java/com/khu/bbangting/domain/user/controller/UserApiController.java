8459cef6b1b79b09f5e7a7f019395727ae3ce329
Hyeyeon-Ko <khy33355@gmail.com>
2023-12-21 00:46:22 +0900

    Refactor: User 컨트롤러, 서비스 수정 및 AuthService 추가

--- backend/src/main/java/com/khu/bbangting/domain/user/controller/UserApiController.java
+++ backend/src/main/java/com/khu/bbangting/domain/user/controller/UserApiController.java
@@ -1,23 +1,29 @@
 package com.khu.bbangting.domain.user.controller;
 
+import com.khu.bbangting.domain.bread.service.BreadService;
 import com.khu.bbangting.domain.user.dto.*;
 import com.khu.bbangting.domain.user.model.User;
+import com.khu.bbangting.domain.user.repository.UserRepository;
+import com.khu.bbangting.domain.user.service.AuthService;
 import com.khu.bbangting.domain.user.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
+import com.khu.bbangting.error.CustomException;
+import com.khu.bbangting.error.ErrorCode;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.http.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class UserApiController {
 
     private final UserService userService;
+    private final AuthService authService;
+    private final BreadService breadService;
+    private final UserRepository userRepository;
+
+    private final long COOKIE_EXPIRATION = 172800; // 2일
 
     // 회원가입
     @PostMapping("/auth/join")
@@ -26,27 +32,93 @@ public class UserApiController {
         return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 성공");
     }
 
-    // 로그인
+    // 로그인 -> 토큰 발급
     @PostMapping("/auth/login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
-        User user = User.builder()
-                .email(requestDto.getEmail())
-                .password(requestDto.getPassword())
+    public ResponseEntity<?> login(@RequestBody LoginRequestDto requestDto) {
+        // User 등록 및 Refresh Token 저장
+        UserTokenDto tokenDto = authService.login(requestDto);
+
+        // RT 저장
+        HttpCookie httpCookie = ResponseCookie.from("Refresh-token", tokenDto.getRefreshToken())
+                .maxAge(COOKIE_EXPIRATION)
+                .httpOnly(true)
+                .secure(true)
                 .build();
 
-        LoginResponseDto responseDto = userService.login(user, response);
+        // 로그인 유저 정보
+        User userDetail = userRepository.findByEmail(requestDto.getEmail())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
-        return ResponseEntity.ok(responseDto);
+        // 로그인 시 -> 팔로우한 스토어의 당일 오픈 빵팅 알림 보내기
+        breadService.loginNotification(userDetail);
+
+        // 로그인 시 -> userId, username 반환
+        LoginResponseDto responseDto = LoginResponseDto.builder()
+                .userId(userDetail.getId())
+                .username(userDetail.getUsername())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .header(HttpHeaders.SET_COOKIE, httpCookie.toString())
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenDto.getAccessToken())
+                .body(responseDto);
     }
 
-    @PostMapping("/auth/refresh-token")
-    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        userService.refreshToken(request, response);
+    @PostMapping("/checkToken")
+    public ResponseEntity<?> validate(@RequestHeader("Authorization") String requestAccessToken) {
+        if (!authService.validate(requestAccessToken)) {
+            return ResponseEntity.status(HttpStatus.OK).build(); // 재발급 필요X
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // 재발급 필요
+        }
     }
 
-    @GetMapping("/checkToken")
-    public void checkToken(@AuthenticationPrincipal User user) {
+    // 토큰 재발급
+    @PostMapping("/refresh")
+    public ResponseEntity<?> reissue(@CookieValue(name = "Refresh-token") String requestRefreshToken,
+                                     @RequestHeader("Authorization") String requestAccessToken) {
+        UserTokenDto reissuedTokenDto = authService.reissue(requestAccessToken, requestRefreshToken);
 
+        if (reissuedTokenDto != null) { // 토큰 재발급 성공
+            // RT 저장
+            ResponseCookie responseCookie = ResponseCookie.from("Refresh-token", reissuedTokenDto.getRefreshToken())
+                    .maxAge(COOKIE_EXPIRATION)
+                    .httpOnly(true)
+                    .secure(true)
+                    .build();
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                    // AT 저장
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + reissuedTokenDto.getAccessToken())
+                    .build();
+
+        } else { // Refresh Token 탈취 가능성
+            // Cookie 삭제 후 재로그인 유도
+            ResponseCookie responseCookie = ResponseCookie.from("Refresh-token", "")
+                    .maxAge(0)
+                    .path("/")
+                    .build();
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                    .build();
+        }
+    }
+
+    // 로그아웃
+    @PostMapping("/auth/logout")
+    public ResponseEntity<?> logout(@RequestHeader("Authorization") String requestAccessToken) {
+        authService.logout(requestAccessToken);
+        ResponseCookie responseCookie = ResponseCookie.from("Refresh-token", "")
+                .maxAge(0)
+                .path("/")
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                .build();
     }
 
 }
