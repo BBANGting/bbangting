7ada22235b7bfd1d9b3e9e947738e45a2a82d14f
Sunny-jinn <rlawlsdn316@gmail.com>
2023-12-21 00:50:18 +0900

    style: global css 통일

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import './styles/global.css';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
